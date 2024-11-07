155da1012f58e6f661545947dc91ad8733301e0f
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-11-07 20:44:18 +0100

    completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 const rootStyles = document.documentElement.style;
+const buttonElemnt = document.getElementById('button')
+const modalElemnt = document.getElementById('modal')
+
+const showModal = () => {
+    modalElemnt.classList.add('modal-show')
+}
+const hideModal = () => {
+    modalElemnt.classList.remove('modal-show')
+}
+
+buttonElemnt.addEventListener('click', showModal)
+modalElemnt.addEventListener('click', hideModal)
+
